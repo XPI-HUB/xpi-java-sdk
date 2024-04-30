a2f5425d8af9c2888d04f2b00d2d18e3051c2d7f
avinashvijayvargiya <avinash14022002@gmail.com>
2024-04-30 23:49:33 +0530

    added task for gradle publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "com.xpi"
@@ -17,3 +18,17 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+publishing.publications {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/octocat/hello-world")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
