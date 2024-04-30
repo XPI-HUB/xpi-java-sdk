ccf94f39fd84f8b50d33f7296cde9d5213d85b5e
avinashvijayvargiya <avinash14022002@gmail.com>
2024-05-01 01:06:38 +0530

    added task for gradle publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,10 +24,6 @@ publishing.publications {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/XPI-HUB/xpi-java-sdk")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GH_TOKEN")
-            }
         }
     }
 }
