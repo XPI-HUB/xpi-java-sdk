18ab8ee338b7475673e3a37fb49f9c9950511fed
avinashvijayvargiya <avinash14022002@gmail.com>
2024-05-01 02:27:04 +0530

    added task for gradle publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    id ("java-library")
     id("maven-publish")
 }
 
@@ -20,22 +20,31 @@ tasks.test {
 }
 
 publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-//        maven(MavenPublication){
-//            from components.java
-//        }
-    }
     repositories {
+        // The target repository
         maven {
+            // Choose whatever name you want
             name = "GitHubPackages"
+            // The url of the repository, where the artifacts will be published
             url = uri("https://maven.pkg.github.com/XPI-HUB/xpi-java-sdk")
-            credentials{
-                username= System.getenv("GITHUB_ACTOR")
-                password= System.getenv("GH_TOKEN")
+            credentials {
+                // The credentials (described in the next section)
+                username = project.findProperty("gpr.user").toString()
+                password = project.findProperty("gpr.key").toString()
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name.set("Project Name")
             }
         }
     }
 }
+
+//val sourcesJar by tasks.registering(Jar::class) {
+//    archiveClassifier.set("sources")
+//    from(sourceSets.main.get().allSource)
+//}
